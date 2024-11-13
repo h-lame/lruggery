3b7ff26cae347fd4793eb2c1c9f1fc886120a560
Murray Steele <muz@h-lame.com>
2024-11-13 12:13:40 -0600

    Update fibers version of names from a hat
    To match what it looked like in [the
    talk](http://h-lame.com/talks/names-from-a-hat)

--- names_from_a_hat/randomize_speakers.rb
+++ names_from_a_hat/randomize_speakers.rb
@@ -4,23 +4,24 @@ random_choice = Fiber.new do |speakers|
   puts "Given: #{speakers}"
   Fiber.yield
   loop do
-    break if speakers.empty?
-    chosen = speakers.delete_at(rand(speakers.size))
-    puts "Chose: #{chosen}"
+    chosen = speakers.delete(speakers.sample)
     Fiber.yield chosen
   end
 end
 
-# Paul wants to use his own laptop, so we'll put him first or last and take him out of the random choice.
-# our_brave_volunteers = ['Paul Ardeleanu', 'Ismael Celis', 'Joel Chippindale', 'Lars Jorgensen', 'Alex MacCaw', 'Anup Narkhede', 'Thomas Pomfret', 'Brent Snook', 'Murray Steele']
-our_brave_volunteers = ['Ismael Celis', 'Joel Chippindale', 'Lars Jorgensen', 'Alex MacCaw', 'Anup Narkhede', 'Thomas Pomfret', 'Brent Snook', 'Murray Steele']
+our_brave_volunteers = [
+  'Murray',
+  'Herve',
+  'Thomas',
+  'Enrique',
+  'Jamis & Adviti',
+  'Aaron'
+]
 
 random_choice.resume our_brave_volunteers
-while random_choice.alive? do
+loop do
   STDIN.gets
   speaker = random_choice.resume
-  applescript = %Q{tell application "Quicksilver" to show large type "#{speaker}"}
-  
-  `osascript -e '#{applescript}'`
+  break if speaker.nil?
+  puts "Chose: #{speaker}"
 end
-
