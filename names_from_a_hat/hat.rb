dedf081b5f8a362c49d88e180afea05b7c39ad6e
Murray Steele <muz@h-lame.com>
2024-11-13 12:49:20 -0600

    Update hat version of names from a hat
    To match more closely what it looked like in [the
    talk](http://h-lame.com/talks/names-from-a-hat)

--- names_from_a_hat/hat.rb
+++ names_from_a_hat/hat.rb
@@ -47,39 +47,39 @@ class Name < Manifestable
   def initialize(handle)
     super()
     @handle = handle
-    @base_size = 20
+    @base_size = 120
     @cur_size = nil
   end
   
   def draw_thyself(x, y, size = @base_size)
-    $app.para(@handle, :font => "Comic Sans MS #{size}px", :top => x, :left => y, :stroke => '#000')
+    $app.para(@handle, :font => "Futura #{size}px", :top => y, :left => x, :stroke => '#000')
   end
-  
-  def size_step
-    @base_size / 24.0
-  end
-  
-  def move_step(the_hat)
-    [(@start_position.first - the_hat.hole.first) / 24.0, (@start_position.last - the_hat.hole.last) / 24.0]
+
+  def move_step(the_hat, scale = 24.0)
+    [
+     (@start_position.first - the_hat.hole.first) / scale,
+     (@start_position.last - the_hat.hole.last) / scale,
+     @base_size / scale
+    ]
   end
   
   def move_towards_the_hat(the_hat)
     begin
       if @shown && @manifestation && !the_hat.contains?(self)
-        step_f = size_step
         @cur_size ||= @base_size
-        @cur_size = @cur_size - step_f
-        step_t, step_l = move_step(the_hat)
+        step_l, step_t, step_size = move_step(the_hat)
         new_x = @manifestation.left - step_l
         new_y = @manifestation.top - step_t
-        if ((@cur_size <= 1) && 
-            (((@manifestation.top - the_hat.hole.first - step_t) <= 0) && 
-             ((@manifestation.left - the_hat.hole.last - step_l) <= 0)))
+        new_size = @cur_size - step_size
+        if ((new_size <= 1) &&
+            (((new_y - the_hat.hole.last).abs <= 1) &&
+             ((new_x - the_hat.hole.first).abs <= 1)))
           the_hat.put_name_into_hat(self)
           self.hide
           return true
         else
-          @manifestation.style(:font => "Futura #{@cur_size}px") if @cur_size > 1
+          @cur_size = new_size
+          @manifestation.style(size: @cur_size)
           @manifestation.move(new_x, new_y)
           return false
         end
@@ -100,115 +100,121 @@ class Hat
     @button = nil
     @magic_star = nil
   end
-  
+
   def put_name_into_hat(the_name)
     @in_the_hat << the_name
   end
-  
+
   def contains?(the_name)
     @in_the_hat.map {|n| n.handle}.include? the_name
   end
-  
-  def button_shown?
-    !@button.nil?
-  end
-  
+
   def draw_button
-    @button = $app.button(MAGIC_WORDS[rand(MAGIC_WORDS.size)], :width => 200, :top => (@hole.last + 150), :left => (@hole.first - 100)) do
+    @button ||= $app.button(MAGIC_WORDS.sample, :width => 200, :top => (@hole.last + 150), :left => (@hole.first - 100)) do
       if $app.winner.nil?
         $app.winner = @chosen
         @in_the_hat.delete(@chosen)
       else
         $app.winner = nil
+        @button.text = MAGIC_WORDS.sample
       end
-      @button.remove
-      draw_button
     end
   end
-  
-  def do_magic
-    @chosen = @in_the_hat[rand(@in_the_hat.size)]
-    @magic_star.move([*(@hole.first-180..@hole.first-140)][rand(40)],[*(@hole.last-125..@hole.last-85)][rand(40)])
+
+  def magic_boundary
+    @magic_boundary ||= {
+      left: (@hole.first-54..@hole.first-14).to_a,
+      top: (@hole.last+15..@hole.last+55).to_a
+    }
   end
-  
+
+  def do_magic
+    @chosen = @in_the_hat.sample
+    @magic_star.move(
+      magic_boundary[:left].sample,
+      magic_boundary[:top].sample
+    )
+  end
+
+  def stop_magic
+    @magic_star.move(@hole.first-34, @hole.last+35)
+  end
+
   def manifest(x, y)
     @hole = [x+100, y+75]
     $app.nostroke
 
     #shadow
-    # fill gray(0.2,1.0)
-    # oval x+40, y+140, 120, 60
+    $app.fill $app.gray(0.2, 0.6)
+    $app.oval left: x+40, top: y+140, width: 120, height: 60
 
     # trunk of hat
     $app.fill '#000'
-    $app.oval x+50, y+50, 100, 40
-    $app.rect x+50, y+70, 100, 100
-    $app.oval x+50, y+150, 100, 40
+    $app.oval left: x+50, top: y+50, width: 100, height: 40
+    $app.rect left: x+50, top: y+70, width: 100, height: 100
+    $app.oval left: x+50, top: y+150, width: 100, height: 40
 
     # white band
     $app.fill '#fff'
-    $app.oval x+50, y+60, 100, 40
-    $app.rect x+50, y+80, 100, 10
-    $app.oval x+50, y+70, 100, 40
+    $app.oval left: x+50, top: y+60, width: 100, height: 40
+    $app.rect left: x+50, top: y+80, width: 100, height: 10
+    $app.oval left: x+50, top: y+70, width: 100, height: 40
     $app.fill '#000'
-    $app.oval x+50, y+60, 100, 40
+    $app.oval left: x+50, top: y+60, width: 100, height: 40
 
     # brim and hole
     $app.fill '#000'
-    $app.oval x+25, y+35, 150, 70
+    $app.oval left: x+25, top: y+35, width: 150, height: 70
     $app.fill '#444'
-    $app.oval x+50, y+53, 100, 34
-    
+    $app.oval left: x+50, top: y+53, width: 100, height: 34
+
     # star - NOTE: wtf? with the - stuff?
     $app.fill '#fd0'
-    @magic_star = $app.star(x-60, y-30, 5, 40)
+    @magic_star = $app.star(left: x+66, top: y+110, points: 7, outer: 35, inner: 18)
   end
 end
 
 Shoes.app :width => 640, :height => 480, :title => 'Names From A Hat' do
-  
   $app = self
-  the_file_path = ask_open_file
-  
-  @names_for_the_hat = File.open(the_file_path) {|f| f.readlines }.map{|l| l.chomp}
-  
-  @book_name_str = @names_for_the_hat.shift
-  
+
+  @names_for_the_hat = File.readlines(ask_open_file, chomp: true)
+  @prize_name = @names_for_the_hat.shift
   @going_in_the_hat = []
   @winner = nil
-  
+
   def show_the_winner
     @book_name.replace(@winner.handle)
   end
-  
-  # TOOD - Work out why didn't attr_accessor work here?
+
+  # TODO - Work out why didn't attr_accessor work here?
   def winner
     @winner
   end
-  
+
   def winner=(winner)
     @winner= winner
-    @book_name.replace(@book_name_str) if @winner.nil?
+    @book_name.replace(@prize_name) if @winner.nil?
   end
-  
+
   background lemonchiffon
-  
+
   @cur_name = nil
-  
+
   # NOTE this is all kinds of ass.  How do real people cope with this stuff?
   def main_loop
     unless @pressed_go.nil?
-      
+
       if @cur_name.nil?
         if !@names_for_the_hat.empty?
           new_name = @names_for_the_hat.pop
           @cur_name = Name.new(new_name)
-          @cur_name.manifest(20, 200)
+          @cur_name.manifest(200, 20)
           @going_in_the_hat << @cur_name
         else
           if @winner.nil?
             @the_hat.do_magic
           else
+            @the_hat.stop_magic
             show_the_winner
           end
         end
@@ -223,7 +229,7 @@ Shoes.app :width => 640, :height => 480, :title => 'Names From A Hat' do
   @the_hat = Hat.new
   @the_hat.manifest(200, 200)
 
-  @book_name = para(@book_name_str, :font => 'Futura 72px', :stroke => indianred, :top => 0, :left => 0)
+  @book_name = para(@prize_name, :font => 'Futura 72px', :stroke => indianred, :top => 0, :left => 0)
 
   @pressed_go = nil
   @the_start_button = button('Shall we begin?', :width => 200, :top => 150, :left => 100) do
