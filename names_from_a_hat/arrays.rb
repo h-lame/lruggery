e0ed8d3384b8e78fa96b3b30015c7df582c043a3
Murray Steele <muz@h-lame.com>
2024-11-24 20:19:06 +0000

    Add the "arrays version" of randmising speakers
    I don't think I ever actually used something like this to randomise speakers
    for a meeting, but I spoke about it in my RubyConf'24 talk "names from a hat",
    so I want this here so I can link to it from the writeup.

---
+++ names_from_a_hat/arrays.rb
@@ -0,0 +1,21 @@
+def pick_a_running_order
+  puts [
+    'Murray',
+    'Herve',
+    'Thomas',
+    'Enrique',
+    'Jamis & Adviti',
+    'Aaron'
+  ].shuffle
+end
+
+def pick_a_winner
+  puts [
+    'Murray',
+    'Herve',
+    'Thomas',
+    'Enrique',
+    'Jamis & Adviti',
+    'Aaron'
+  ].shuffle
+end
